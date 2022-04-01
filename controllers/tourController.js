06a3591d34acc9dea553320589a223f4af33fe22
unknown <yaredbest81@gmail.com>
2022-04-01 23:03:10 +0300

    sort function is added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const match = require('nodemon/lib/monitor/match');
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
@@ -7,20 +8,30 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
     console.log(req.query, queryObj);
-
-    const query = Tour.find(queryObj);
-
     //{difficulty: 'easy', duration: {$gte: 5}}
-
+    ///ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
     // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
 
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query.sort('-createdAt');
+    }
+
     //excute query
     const tours = await query;
-
     //send response
     res.status(200).json({
       status: 'success',
