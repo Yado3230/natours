1f9ff3910ca5595e6adb7e0a9781b66abf932285
unknown <yaredbest81@gmail.com>
2022-04-01 23:55:11 +0300

    added field limiting and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,10 +27,30 @@ exports.getAllTours = async (req, res) => {
       console.log(sortBy);
       query = query.sort(sortBy);
     } else {
-      query.sort('-createdAt');
+      query = query.sort('-createdAt');
     }
 
+    //field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await TouchList.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
     //excute query
+    //query.sort().select().skip().limit()
     const tours = await query;
     //send response
     res.status(200).json({
